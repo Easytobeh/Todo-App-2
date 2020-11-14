957eb681ba7ccfc7dd0f840e9ccf02032fbbfed7
Junaid Oloruntobi <easytobeh@gmail.com>
2020-11-14 17:01:48 +0100

    Completed Update and  Delete Todo Item

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
 
 document.getElementById('btn').addEventListener('click', addNewItem);
 
-let todos = document.querySelectorAll('li');
+//Add event listener to all todo item
+let todos = document.querySelector('.todos');
 
-//console.log (todos);
-addListener();
+todos.addEventListener('click', (event) => {
+    //  console.log(event.target);
+    // console.log(event.target.parentElement);
+    let buttonFunction = event.target.innerHTML;
 
-function addListener() {
-//Add event listener to each button; del and done
-    todos.forEach(todo => {
-       // console.log(todo.children);
-    
-        for(let child of todo.children) 
-            child.addEventListener('click', function(event) {
-                  console.log(event.target);
-            })
-    
-    });
-}
+    if (buttonFunction === 'Delete')
+        confirm("Are you sure?") ? todos.removeChild(event.target.parentElement) : null;
+
+    else if (buttonFunction === 'Done') {
+
+        event.target.parentElement.style.textDecoration = 'line-through';
+        //event.target.style.setProperty('display', 'none');
+        event.target.innerHTML = 'Undo';
+    }
+
+    else if (buttonFunction === 'Undo') {
+        event.target.parentElement.style.textDecoration = 'none';
+        event.target.innerHTML = 'Done';
+    }
+
+});
 
 
-
-function addNewItem () {
+function addNewItem() {
     let newTodo = document.getElementById('new-item').value;
     //console.log(`New todo ${newTodo}`);
-    
-    //create new element
-    let element = document.createElement("li");
-    let elementNode = document.createTextNode(newTodo);
-    element.appendChild(elementNode);
 
-      //Update list
-      let newItem = document.getElementById('todos');
-      newItem.appendChild(element);
+    if (newTodo !== '') {
+        //create new element
+        let element = document.createElement("li");
+        let elementNode = document.createTextNode(newTodo);
+        element.appendChild(elementNode);
 
-    //create delete and done button
-    let del = document.createElement('button');
-    let done = document.createElement('button');
-    done.className = 'done-btn';
-    del.className = 'dlt-btn';
-    del.appendChild(document.createTextNode('Delete'));
-    done.appendChild(document.createTextNode('Done'));
-    element.appendChild(done);
-    newItem.appendChild(element);
-    
-    element.appendChild(del);
-    newItem.appendChild(element);
-    //console.log(`New Item: ${newTodo}`);
+        //Update list
+        let newItem = document.getElementById('todos');
+        newItem.appendChild(element);
 
-    //clear input text area
-    let reset = document.getElementById('new-item').value = '';
-    addListener();
+        //create delete and done button
+        let del = document.createElement('button');
+        let done = document.createElement('button');
+        done.className = 'done-btn';
+        del.className = 'dlt-btn';
+        del.appendChild(document.createTextNode('Delete'));
+        done.appendChild(document.createTextNode('Done'));
+        element.appendChild(done);
+        newItem.appendChild(element);
+
+        element.appendChild(del);
+        newItem.appendChild(element);
+        //add listener to list again
+
+        //console.log(`New Item: ${newTodo}`);
+
+        //clear input text area
+        let reset = document.getElementById('new-item').value = '';
+    }
+
 }
